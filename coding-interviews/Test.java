68446bc2ccafba986431582537fc2f2b20de0848
wangjie <jiebupup@gmail.com>
2020-01-10 15:51:42 +0800

    commit

--- coding-interviews/Test.java
+++ coding-interviews/Test.java
@@ -1,21 +1,15 @@
 public class Test {
-    public ListNode deleteNode(ListNode head, ListNode tobeDelete) {
-        if (head == null || tobeDelete == null) return null;
-        if (tobeDelete.next != null) {
-            ListNode next = tobeDelete.next;
-            tobeDelete.val = next.val;
-            tobeDelete.next = next.next;
+    public ListNode deleteDuplication(ListNode pHead) {
+        if (pHead == null || pHead.next == null)
+            return pHead;
+        ListNode next = pHead.next;
+        if (pHead.val == next.val) {
+            while (next != null && next.val == pHead.val)
+                next = next.next;
+            return deleteDuplication(next);
         } else {
-            if (head == tobeDelete)
-                head = null;
-            else {
-                ListNode cur = head;
-                while (cur.next != tobeDelete) {
-                    cur = cur.next;
-                }
-                cur.next = null;
-            }
+            pHead.next = deleteDuplication(pHead.next);
+            return pHead;
         }
-        return head;
     }
 }
