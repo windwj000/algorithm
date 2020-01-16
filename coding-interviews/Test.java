7ecec147a8cf853875cb1bf13db2cc743ca2296e
wangjie <jiebupup@gmail.com>
2020-01-16 23:28:54 +0800

    commit

--- coding-interviews/Test.java
+++ coding-interviews/Test.java
@@ -1,7 +1,21 @@
 public class Test {
-    public boolean isNumeric(char[] str) {
-        if (str == null || str.length == 0)
-            return false;
-        return new String(str).matches("[+-]?\\d*(\\.\\d+)?([eE][+-]?\\d+)?");
+    public void reOrderArray(int[] array) {
+        int[] tmp = new int[array.length];
+        int idx = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (!isEven(array[i]))
+                tmp[idx++] = array[i];
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (isEven(array[i]))
+                tmp[idx++] = array[i];
+        }
+        for (int i = 0; i < array.length; i++) {
+            array[i] = tmp[i];
+        }
+    }
+
+    private boolean isEven(int n) {
+        return (n & 1) == 0;
     }
 }
