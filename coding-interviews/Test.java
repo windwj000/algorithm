1a6a6327644c6f59858e8b509fe08f8daebbf619
wangjie <jiebupup@gmail.com>
2020-02-07 11:04:07 +0800

    commit

--- coding-interviews/Test.java
+++ coding-interviews/Test.java
@@ -5,46 +5,23 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class Test {
-    private class Node {
-        int val;
-        int min;
-        Node next;
-
-        private Node(int val, int min) {
-            this(val, min, null);
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int cnt = q.size();
+            while (cnt-- > 0) {
+                TreeNode p = q.poll();
+                if (p == null)
+                    continue;
+                res.add(p.val);
+                q.offer(p.left);
+                q.offer(p.right);
+            }
         }
-
-        private Node(int val, int min, Node next) {
-            this.val = val;
-            this.min = min;
-            this.next = next;
-        }
-    }
-
-    private Node head;
-
-    /**
-     * initialize your data structure here.
-     */
-    public MinStack() {
-    }
-
-    public void push(int x) {
-        if (head == null) {
-            head = new Node(x, x);
-        } else
-            head = new Node(x, Math.min(x, head.min), head);
-    }
-
-    public void pop() {
-        head = head.next;
-    }
-
-    public int top() {
-        return head.val;
-    }
-
-    public int getMin() {
-        return head.min;
+        return res;
     }
 }
