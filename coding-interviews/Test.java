21de9675782e92076e91dc3a543fff11f3ee36ce
wangjie <jiebupup@gmail.com>
2020-02-11 23:04:28 +0800

    commit

--- coding-interviews/Test.java
+++ coding-interviews/Test.java
@@ -2,25 +2,19 @@ import javax.swing.tree.TreeNode;
 import java.util.*;
 
 public class Test {
-    public ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+    ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
         ArrayList<ArrayList<Integer>> res = new ArrayList<>();
         f(pRoot, res, 0);
         return res;
     }
 
-    private void f(TreeNode node, ArrayList<ArrayList<Integer>> res, int level) {
-        if (node == null)
+    private void f(TreeNode pRoot, ArrayList<ArrayList<Integer>> res, int depth) {
+        if (pRoot == null)
             return;
-        if (level == res.size()) {
-            ArrayList<Integer> newLevel = new ArrayList<>();
-            res.add(newLevel);
-        }
-        ArrayList<Integer> tmp = res.get(level);
-        if (level % 2 == 0)
-            tmp.add(node.val);
-        else
-            tmp.add(0, node.val);
-        f(node.left, res, level + 1);
-        f(node.right, res, level + 1);
+        if (depth == res.size())
+            res.add(new ArrayList<Integer>());
+        res.get(depth).add(pRoot.val);
+        f(pRoot.left, res, depth + 1);
+        f(pRoot.right, res, depth + 1);
     }
 }
