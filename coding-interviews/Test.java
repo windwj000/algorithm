33208cf8386fd83a63d7670d6cdc97482b6d5a60
wangjie <jiebupup@gmail.com>
2020-02-20 19:26:15 +0800

    commit

--- coding-interviews/Test.java
+++ coding-interviews/Test.java
@@ -2,30 +2,45 @@ import javax.swing.tree.TreeNode;
 import java.util.*;
 
 public class Test {
-    public RandomListNode Clone(RandomListNode pHead) {
-        if (pHead == null)
+    public String Serialize(TreeNode root) {
+        if (root == null)
+            return "";
+        Queue<TreeNode> q = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode n = q.poll();
+            if (n == null) {
+                sb.append("#").append(",");
+                continue;
+            }
+            sb.append(n.val).append(",");
+            q.offer(n.left);
+            q.offer(n.right);
+        }
+        return sb.toString();
+    }
+
+    public TreeNode Deserialize(String str) {
+        if (str == "")
             return null;
-        RandomListNode cur = pHead;
-        while (cur != null) {
-            RandomListNode clone = new RandomListNode(cur.label);
-            clone.next = cur.next;
-            cur.next = clone;
-            cur = clone.next;
+        String[] sa = str.split(",");
+        Queue<TreeNode> q = new LinkedList<>();
+        TreeNode root = new TreeNode(Integer.valueOf(sa[0]));
+        q.offer(root);
+        for (int i = 1; i < sa.length; i++) {
+            TreeNode n = q.poll();
+            if (!sa[i].equals("#")) {
+                TreeNode left = new TreeNode(Integer.valueOf(sa[i]));
+                n.left = left;
+                q.offer(left);
+            }
+            if (!sa[++i].equals("#")) {
+                TreeNode right = new TreeNode(Integer.valueOf(sa[i]));
+                n.right = right;
+                q.offer(right);
+            }
         }
-        cur = pHead;
-        while (cur != null) {
-            RandomListNode clone = cur.next;
-            if (cur.random != null)
-                clone.random = cur.random.next;
-            cur = clone.next;
-        }
-        cur = pHead;
-        RandomListNode cloneHead = pHead.next;
-        while (cur.next != null) {
-            RandomListNode next = cur.next;
-            cur.next = next.next;
-            cur = next;
-        }
-        return cloneHead;
+        return root;
     }
 }
