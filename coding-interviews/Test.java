2cc463e5406176b9e8dbf90a162fd648736b9889
wangjie <jiebupup@gmail.com>
2020-03-10 12:00:58 +0800

    commit

--- coding-interviews/Test.java
+++ coding-interviews/Test.java
@@ -2,20 +2,16 @@ import javax.swing.tree.TreeNode;
 import java.util.*;
 
 public class Test {
-    public int GetUglyNumber_Solution(int index) {
-        if (index < 7)
-            return index;
-        int[] dp = new int[index];
-        dp[0] = 1;
-        for (int i = 1, two = 0, three = 0, five = 0; i < index; i++) {
-            dp[i] = Math.min(dp[two] * 2, Math.min(dp[three] * 3, dp[five] * 5));
-            if (dp[i] == dp[two] * 2)
-                two++;
-            if (dp[i] == dp[three] * 3)
-                three++;
-            if (dp[i] == dp[five] * 5)
-                five++;
+    public int lengthOfLongestSubstring(String s) {
+        int res = 0;
+        Queue<Character> q = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            while (q.contains(c)) {
+                q.poll();
+            }
+            q.offer(c);
+            res = Math.max(res, q.size());
         }
-        return dp[index - 1];
+        return res;
     }
 }
