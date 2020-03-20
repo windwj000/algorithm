6814fe4810d913e4318002dbfc658c9cfe5a36f4
wangjie <jiebupup@gmail.com>
2020-03-20 23:17:49 +0800

    commit

--- coding-interviews/Test.java
+++ coding-interviews/Test.java
@@ -2,30 +2,22 @@ import javax.swing.tree.TreeNode;
 import java.util.*;
 
 public class Test {
-    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
-        if (pHead1 == null || pHead2 == null)
-            return null;
-        ListNode last = pHead1;
-        while (last.next != null) {
-            last = last.next;
+    public int GetNumberOfK(int[] array, int k) {
+        int first = binarySearch(array, k);
+        int last = binarySearch(array, k + 1);
+        return last - first;
+    }
+
+    private int binarySearch(int[] array, int k) {
+        int low = 0;
+        int high = array.length;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (array[mid] < k)
+                low = mid + 1;
+            else
+                high = mid;
         }
-        last.next = pHead2;
-        ListNode slow = pHead1;
-        ListNode fast = pHead1;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-            if (slow == fast) {
-                fast = pHead1;
-                while (slow != fast) {
-                    slow = slow.next;
-                    fast = fast.next;
-                }
-                last.next = null;
-                return slow;
-            }
-        }
-        last.next = null;
-        return null;
+        return low;
     }
 }
