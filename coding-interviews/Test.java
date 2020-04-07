45ec0a63887148dcc8c3df90467bcfc68a9dc7c8
wangjie <jiebupup@gmail.com>
2020-04-07 12:58:52 +0800

    commit

--- coding-interviews/Test.java
+++ coding-interviews/Test.java
@@ -2,29 +2,13 @@ import javax.swing.tree.TreeNode;
 import java.util.*;
 
 public class Test {
-    public String ReverseSentence(String str) {
+    public String LeftRotateString(String str, int n) {
         int len = str.length();
-        char[] c = str.toCharArray();
-        int i = 0;
-        for (int j = 0; j <= len; j++) {
-            if (j == len || str.charAt(j) == ' ') {
-                reverse(c, i, j - 1);
-                i = j + 1;
-            }
-        }
-        reverse(c, 0, len - 1);
-        return new String(c);
-    }
-
-    private void reverse(char[] c, int i, int j) {
-        while (i < j) {
-            swap(c, i++, j--);
-        }
-    }
-
-    private void swap(char[] c, int i, int j) {
-        char tmp = c[i];
-        c[i] = c[j];
-        c[j] = tmp;
+        if (str == null || len == 0)
+            return "";
+        if (n >= len)
+            return str;
+        str += str;
+        return str.substring(n, n + len);
     }
 }
