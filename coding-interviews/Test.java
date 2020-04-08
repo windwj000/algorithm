5d6745453acd920c003f444bd2235bafbaf25379
wangjie <jiebupup@gmail.com>
2020-04-08 17:06:03 +0800

    commit

--- coding-interviews/Test.java
+++ coding-interviews/Test.java
@@ -2,24 +2,24 @@ import javax.swing.tree.TreeNode;
 import java.util.*;
 
 public class Test {
-    public double[] twoSum(int n) {
-        int[] dp = new int[6 * n + 1];
-        for (int i = 1; i <= 6; i++) {
-            dp[i] = 1;
+    public boolean isContinuous(int[] numbers) {
+        int len = numbers.length;
+        if (numbers == null || len == 0 || len != 5)
+            return false;
+        int max = -1;
+        int min = 14;
+        int flag = 0;
+        for (int n : numbers) {
+            if (n == 0)
+                continue;
+            if (((flag >> n) & 1) == 1)
+                return false;
+            flag |= (1 << n);
+            if (n > max)
+                max = n;
+            if (n < min)
+                min = n;
         }
-        for (int i = 2; i <= n; i++) {
-            for (int j = 6 * i; j >= i; j--) {
-                dp[j] = 0;
-                for (int k = 1; k <= 6 && j - k >= i - 1; k++) {
-                    dp[j] += dp[j - k];
-                }
-            }
-        }
-        double total = Math.pow(6, n);
-        double[] res = new double[5 * n + 1];
-        for (int i = n; i <= 6 * n; i++) {
-            res[i - n] = dp[i] / total;
-        }
-        return res;
+        return max - min < 5;
     }
 }
