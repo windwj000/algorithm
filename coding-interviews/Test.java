a2e650c8377eab243ffb9a2e0df47c53fcf21978
wangjie <jiebupup@gmail.com>
2020-04-13 15:00:33 +0800

    commit

--- coding-interviews/Test.java
+++ coding-interviews/Test.java
@@ -2,18 +2,7 @@ import javax.swing.tree.TreeNode;
 import java.util.*;
 
 public class Test {
-    public int maxProfit(int[] prices) {
-        int len = prices.length;
-        if (prices == null || len < 2)
-            return 0;
-        int min = prices[0];
-        int max = 0;
-        for (int i = 1; i < len; i++) {
-            if (prices[i] < prices[i - 1])
-                min = Math.min(min, prices[i]);
-            else
-                max = Math.max(max, prices[i] - min);
-        }
-        return max;
+    public int Add(int num1, int num2) {
+        return num2 == 0 ? num1 : Add(num1 ^ num2, (num1 & num2) << 1);
     }
 }
