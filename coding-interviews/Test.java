d591aeb4e6d86b2c9c36c6ee86fa40183e0d2ff8
wangjie <jiebupup@gmail.com>
2020-04-15 23:16:58 +0800

    commit

--- coding-interviews/Test.java
+++ coding-interviews/Test.java
@@ -1,8 +1,17 @@
-import javax.swing.tree.TreeNode;
-import java.util.*;
-
 public class Test {
-    public int Add(int num1, int num2) {
-        return num2 == 0 ? num1 : Add(num1 ^ num2, (num1 & num2) << 1);
+    public int[] multiply(int[] A) {
+        int len = A.length;
+        int[] B = new int[len];
+        if (A == null || len == 0)
+            return B;
+        B[0] = 1;
+        for (int i = 1; i < len; i++) {
+            B[i] = B[i - 1] * A[i - 1];
+        }
+        for (int i = len - 2, tmp = 1; i >= 0; i--) {
+            tmp *= A[i + 1];
+            B[i] *= tmp;
+        }
+        return B;
     }
 }
