6560c5038f61b89a50a503d8d8a10f9b1fb037dc
wangjie <jiebupup@gmail.com>
2020-04-24 15:14:41 +0800

    commit

--- coding-interviews/Test.java
+++ coding-interviews/Test.java
@@ -1,35 +1,27 @@
+import javax.swing.tree.TreeNode;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Stack;
+
 public class Test {
-    public int StrToInt(String str) {
-        int len = str.length();
-        if (str == null || len == 0)
-            return 0;
-        int res = 0;
-        boolean negative = false;
-        int limit = -Integer.MAX_VALUE;
-        int i = 0;
-        char firstChar = str.charAt(0);
-        if (firstChar < '0') {
-            if (firstChar == '-') {
-                negative = true;
-                limit = Integer.MIN_VALUE;
-            } else if (firstChar != '+')
-                return 0;
-            if (len == 1)
-                return 0;
-            i++;
+    public boolean duplicate(int numbers[], int length, int[] duplication) {
+        if (numbers == null || length == 0)
+            return false;
+        for (int i = 0; i < length; i++) {
+            while (numbers[i] != i) {
+                if (numbers[i] == numbers[numbers[i]]) {
+                    duplication[0] = numbers[i];
+                    return true;
+                }
+                swap(numbers, i, numbers[i]);
+            }
         }
-        int multmin = limit / 10;
-        while (i < len) {
-            int digit = str.charAt(i++) - '0';
-            if (digit < 0 || digit > 9)
-                return 0;
-            if (res < multmin)
-                return 0;
-            res *= 10;
-            if (res < limit + digit)
-                return 0;
-            res -= digit;
-        }
-        return negative ? res : -res;
+        return false;
+    }
+
+    private void swap(int[] numbers, int i, int j) {
+        int tmp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = tmp;
     }
 }
