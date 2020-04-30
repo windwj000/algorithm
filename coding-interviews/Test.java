e3d9a397e1e034f48902cde2f01805a21c041b79
wangjie <jiebupup@gmail.com>
2020-04-30 15:03:58 +0800

    commit

--- coding-interviews/Test.java
+++ coding-interviews/Test.java
@@ -5,16 +5,29 @@ import java.util.HashSet;
 import java.util.Stack;
 
 public class Test {
-    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
-        Stack<Integer> stack = new Stack<>();
-        while (listNode != null) {
-            stack.push(listNode.val);
-            listNode = listNode.next;
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < in.length; i++) {
+            map.put(in[i], i);
         }
-        ArrayList<Integer> res = new ArrayList<>();
-        while (!stack.isEmpty()) {
-            res.add(stack.pop());
+        Stack<TreeNode> stack = new Stack<>();
+        int val = pre[0];
+        TreeNode root = new TreeNode(val);
+        stack.push(root);
+        for (int i = 1; i < pre.length; i++) {
+            val = pre[i];
+            TreeNode node = new TreeNode(val);
+            if (map.get(val) < map.get(stack.peek().val))
+                stack.peek().left = node;
+            else {
+                TreeNode parent = null;
+                while (!stack.isEmpty() && map.get(val) > map.get(stack.peek().val)) {
+                    parent = stack.pop();
+                }
+                parent.right = node;
+            }
+            stack.push(node);
         }
-        return res;
+        return root;
     }
 }
