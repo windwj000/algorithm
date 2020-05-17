177ee9dd4fde59ef6d0253cc23a56bf810e989ef
wangjie <jiebupup@gmail.com>
2020-05-17 16:58:58 +0800

    commit

--- coding_interviews/Test.java
+++ coding_interviews/Test.java
@@ -5,26 +5,30 @@ import java.util.HashSet;
 import java.util.Stack;
 
 public class Test {
-    public int minNumberInRotateArray(int[] array) {
-        int low = 0;
-        int high = array.length - 1;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (array[low] == array[mid] && array[mid] == array[high])
-                return minNumber(array, low, high);
-            if (array[mid] > array[high])
-                low = mid + 1;
-            else
-                high = mid;
+    public boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
+        boolean[] flag = new boolean[matrix.length];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (backtracking(matrix, rows, cols, str, i, j, flag, 0))
+                    return true;
+            }
         }
-        return array[low];
+        return false;
     }
 
-    private int minNumber(int[] array, int low, int high) {
-        for (int i = low; i < high; i++) {
-            if (array[i] > array[i + 1])
-                return array[i + 1];
-        }
-        return array[low];
+    private boolean backtracking(char[] matrix, int rows, int cols, char[] str, int i, int j, boolean[] flag, int idx) {
+        int tmp = i * cols + j;
+        if (i < 0 || i >= rows || j < 0 || j >= cols || matrix[tmp] != str[idx] || flag[tmp] == true)
+            return false;
+        if (idx == str.length - 1)
+            return true;
+        flag[tmp] = true;
+        if (backtracking(matrix, rows, cols, str, i + 1, j, flag, idx + 1) ||
+                backtracking(matrix, rows, cols, str, i - 1, j, flag, idx + 1) ||
+                backtracking(matrix, rows, cols, str, i, j + 1, flag, idx + 1) ||
+                backtracking(matrix, rows, cols, str, i, j - 1, flag, idx + 1))
+            return true;
+        flag[tmp] = false;
+        return false;
     }
 }
