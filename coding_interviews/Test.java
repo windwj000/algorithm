a0d899d394adca0747bb8806b7bdae5aac0acd82
wangjie <jiebupup@gmail.com>
2020-06-01 16:14:22 +0800

    commit

--- coding_interviews/Test.java
+++ coding_interviews/Test.java
@@ -5,30 +5,14 @@ import java.util.HashSet;
 import java.util.Stack;
 
 public class Test {
-    public boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
-        boolean[] flag = new boolean[matrix.length];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (backtracking(matrix, rows, cols, str, i, j, flag, 0))
-                    return true;
+    public int cutRope(int target) {
+        int[] dp = new int[target + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= target; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), dp[j] * (i - j)));
             }
         }
-        return false;
-    }
-
-    private boolean backtracking(char[] matrix, int rows, int cols, char[] str, int i, int j, boolean[] flag, int idx) {
-        int tmp = i * cols + j;
-        if (i < 0 || i >= rows || j < 0 || j >= cols || matrix[tmp] != str[idx] || flag[tmp] == true)
-            return false;
-        if (idx == str.length - 1)
-            return true;
-        flag[tmp] = true;
-        if (backtracking(matrix, rows, cols, str, i + 1, j, flag, idx + 1) ||
-                backtracking(matrix, rows, cols, str, i - 1, j, flag, idx + 1) ||
-                backtracking(matrix, rows, cols, str, i, j + 1, flag, idx + 1) ||
-                backtracking(matrix, rows, cols, str, i, j - 1, flag, idx + 1))
-            return true;
-        flag[tmp] = false;
-        return false;
+        return dp[target];
     }
 }
