7ff0fe91c670ba24ca5e5697f8929e173b6bbcad
wangjie <jiebupup@gmail.com>
2020-07-20 00:21:58 +0800

    commit

--- coding_interviews/Test.java
+++ coding_interviews/Test.java
@@ -5,19 +5,32 @@ import java.util.HashSet;
 import java.util.Stack;
 
 public class Test {
-    public ListNode deleteDuplication(ListNode pHead) {
-        if (pHead == null || pHead.next == null) {
-            return pHead;
+    public boolean match(char[] str, char[] pattern) {
+        if (str == null || pattern == null) {
+            return false;
         }
-        ListNode next = pHead.next;
-        if (pHead.val == next.val) {
-            while (next != null && next.val == pHead.val) {
-                next = next.next;
+        int len1 = str.length;
+        int len2 = pattern.length;
+        boolean[][] dp = new boolean[len1 + 1][len2 + 1];
+        dp[0][0] = true;
+        for (int i = 1; i < len2; i++) {
+            if (pattern[i] == '*' && dp[0][i - 1]) {
+                dp[0][i + 1] = true;
             }
-            return deleteDuplication(next);
-        } else {
-            pHead.next = deleteDuplication(pHead.next);
-            return pHead;
         }
+        for (int i = 0; i < len1; i++) {
+            for (int j = 0; j < len2; j++) {
+                if (pattern[j] == '*') {
+                    if (pattern[j - 1] == '.' || str[i] == pattern[j - 1]) {
+                        dp[i + 1][j + 1] = dp[i][j + 1] || dp[i + 1][j - 1];
+                    } else {
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                    }
+                } else if (pattern[j] == '.' || str[i] == pattern[j]) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+            }
+        }
+        return dp[len1][len2];
     }
 }
