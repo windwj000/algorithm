2a1d29ee9dc9b1167c586bb90d57dfed7194acf3
wangjie <jiebupup@gmail.com>
2021-03-08 15:58:14 +0800

    commit

--- coding_interviews/Test.java
+++ coding_interviews/Test.java
@@ -1,32 +1,17 @@
 import javax.swing.tree.TreeNode;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Stack;
+import java.util.*;
 
 public class Test {
-    public boolean isMatch(String s, String p) {
-        int len1 = s.length();
-        int len2 = p.length();
-        boolean[][] dp = new boolean[len1 + 1][len2 + 1];
-        dp[len1][len2] = true;
-        for (int i = len2 - 1; i >= 0; i--) {
-            if (p.charAt(i) != '*') {
-                break;
-            } else {
-                dp[s.length()][i] = true;
+    public int FirstNotRepeatingChar(String str) {
+        HashMap<Character, Boolean> hm = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            hm.put(c, !hm.containsKey(c));
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (hm.get(str.charAt(i))) {
+                return i;
             }
         }
-        for (int i = s.length() - 1; i >= 0; i--) {
-            for (int j = p.length() - 1; j >= 0; j--) {
-                if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')
-                    dp[i][j] = dp[i + 1][j + 1];
-                else if (p.charAt(j) == '*')
-                    dp[i][j] = dp[i + 1][j] || dp[i][j + 1];
-                else
-                    dp[i][j] = false;
-            }
-        }
-        return dp[0][0];
+        return -1;
     }
 }
