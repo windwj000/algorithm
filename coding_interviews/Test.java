0550c6e1d01ea3e8364813370452e2e1a5a9e7f9
wangjie <jiebupup@gmail.com>
2021-03-10 21:32:55 +0800

    commit

--- coding_interviews/Test.java
+++ coding_interviews/Test.java
@@ -1,17 +1,21 @@
-import javax.swing.tree.TreeNode;
 import java.util.*;
 
 public class Test {
-    public int FirstNotRepeatingChar(String str) {
-        HashMap<Character, Boolean> hm = new HashMap<>();
-        for (char c : str.toCharArray()) {
-            hm.put(c, !hm.containsKey(c));
+    public ArrayList<Integer> maxInWindows(int[] num, int size) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (num == null || num.length == 0 || size <= 0 || size > num.length) {
+            return res;
         }
-        for (int i = 0; i < str.length(); i++) {
-            if (hm.get(str.charAt(i))) {
-                return i;
-            }
+        PriorityQueue<Integer> maxheap = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        for (int i = 0; i < size; i++) {
+            maxheap.offer(num[i]);
         }
-        return -1;
+        res.add(maxheap.peek());
+        for (int i = size; i < num.length; i++) {
+            maxheap.remove(num[i - size]);  //注意，先 remove()
+            maxheap.offer(num[i]);
+            res.add(maxheap.peek());
+        }
+        return res;
     }
 }
