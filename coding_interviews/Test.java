9ec28ca84f07e0a0bb91adc2cc908c49a248c547
wangjie <jiebupup@gmail.com>
2021-03-17 11:22:49 +0800

    commit

--- coding_interviews/Test.java
+++ coding_interviews/Test.java
@@ -1,21 +1,18 @@
 import java.util.*;
 
 public class Test {
-    public ArrayList<Integer> maxInWindows(int[] num, int size) {
-        ArrayList<Integer> res = new ArrayList<>();
-        if (num == null || num.length == 0 || size <= 0 || size > num.length) {
-            return res;
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        HashSet<ListNode> hs = new HashSet<>();
+        while (pHead1 != null) {
+            hs.add(pHead1);
+            pHead1 = pHead1.next;
         }
-        PriorityQueue<Integer> maxheap = new PriorityQueue<>((o1, o2) -> o2 - o1);
-        for (int i = 0; i < size; i++) {
-            maxheap.offer(num[i]);
+        while (pHead2 != null) {
+            if (hs.contains(pHead2)) {
+                return pHead2;
+            }
+            pHead2 = pHead2.next;
         }
-        res.add(maxheap.peek());
-        for (int i = size; i < num.length; i++) {
-            maxheap.remove(num[i - size]);  //注意，先 remove()
-            maxheap.offer(num[i]);
-            res.add(maxheap.peek());
-        }
-        return res;
+        return null;
     }
 }
