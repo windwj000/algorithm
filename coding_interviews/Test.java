ebc9acc41f4ad60961c86775f4f54d20f6bf39e4
wangjie <jiebupup@gmail.com>
2021-03-18 20:26:47 +0800

    commit

--- coding_interviews/Test.java
+++ coding_interviews/Test.java
@@ -1,18 +1,28 @@
+import javax.swing.tree.TreeNode;
 import java.util.*;
 
 public class Test {
-    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
-        HashSet<ListNode> hs = new HashSet<>();
-        while (pHead1 != null) {
-            hs.add(pHead1);
-            pHead1 = pHead1.next;
+    public ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        dfs(pRoot, res, 0);
+        return res;
+    }
+
+    private void dfs(TreeNode node, ArrayList<ArrayList<Integer>> res, int level) {
+        if (node == null) {
+            return;
         }
-        while (pHead2 != null) {
-            if (hs.contains(pHead2)) {
-                return pHead2;
-            }
-            pHead2 = pHead2.next;
+        if (level == res.size()) {
+            ArrayList<Integer> newLevel = new ArrayList<>();
+            res.add(newLevel);
         }
-        return null;
+        ArrayList<Integer> tmp = res.get(level);
+        if (level % 2 == 0) {
+            tmp.add(node.val);
+        } else {
+            tmp.add(0, node.val);
+        }
+        dfs(node.left, res, level + 1);
+        dfs(node.right, res, level + 1);
     }
 }
